6de30da40acbbdec71314915dcd6dee741323969
ttjoe <ttjoe0@gmail.com>
2023-04-12 18:16:53 +0000

    [UPDATE] Create  folders, change port, set vite alias, clean vite files

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+      <div>
+          <h1>FIRESPOT</h1>
+          <p>Fire detection app for everyone</p>
+      </div>
+  )
+}
+
+export default HomePage
