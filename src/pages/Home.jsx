a14c1e9cec486c26bdc18f08a8d97be55b8689d4
ttjoe <ttjoe0@gmail.com>
2023-04-14 18:51:23 +0000

    [FEATURE] Fetch Data from NASA Satelite and display on page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import styled from "styled-components"
+import Paper from '@mui/material/Paper';
+import FireFeed from '@/FireFeed';
 
 function Home() {
     return (
         <>
-            <h1>FIRESPOT</h1>
-            <p>Fire Detection app for everyone</p>
+            <FireFeed />
         </>
-
     )
 }
 
