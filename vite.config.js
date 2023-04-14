839faac41c0eaef3d4f581972658c826a0a9ac44
ttjoe <ttjoe0@gmail.com>
2023-04-14 11:48:21 +0000

    [UPDATE] Change project language from TypeScript to JavaScript

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@pages",
+        replacement: path.resolve(__dirname, "src/pages"),
+      },
+      {
+        find: "@utils",
+        replacement: path.resolve(__dirname, "src/utils"),
+      },
+      {
+        find: "@helpers",
+        replacement: path.resolve(__dirname, "src/helpers"),
+      },
+      {
+        find: "@hooks",
+        replacement: path.resolve(__dirname, "src/hooks"),
+      },
+      {
+        find: "@assets",
+        replacement: path.resolve(__dirname, "src/assets"),
+      },
+      {
+        find: "@src",
+        replacement: path.resolve(__dirname, "src"),
+      },
+      {
+        find: "@contexts",
+        replacement: path.resolve(__dirname, "src/contexts"),
+      },
+      {
+        find: "@config",
+        replacement: path.resolve(__dirname, "src/config"),
+      },
+    ],
+  },
+  server: {
+    port: 5050,
+    open: true,
+  },
+});
