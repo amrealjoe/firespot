7e77d867624469a7d21f0494d3539ea7e2b7a51b
ttjoe <ttjoe0@gmail.com>
2023-04-14 16:24:44 +0000

    [FEATURE] Create generic Navigation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <h1>FIRESPOT</h1>
+            <p>Fire Detection app for everyone</p>
+        </>
+
+    )
+}
+
+export default Home
