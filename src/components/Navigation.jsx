1ae89a1742262c511e1b81f028b13c7260342a34
ttjoe <ttjoe0@gmail.com>
2023-04-14 11:23:16 +0000

    [UPDATE] Setup folder structure, aliases, clean file etc.

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Navigation() {
+    return (
+        <div>Navigation</div>
+    )
+}
+
+export default Navigation
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@hooks",
+        replacement: path.resolve(__dirname, "src/hooks"),
+      },
+      {
+        find: "@contexts",
+        replacement: path.resolve(__dirname, "src/contexts"),
+      },
+      {
+        find: "@helpers",
+        replacement: path.resolve(__dirname, "src/helpers"),
+      },
+      {
+        find: "@config",
+        replacement: path.resolve(__dirname, "src/config"),
+      },
+      {
+        find: "@assets",
+        replacement: path.resolve(__dirname, "src/assets"),
+      },
+    ],
+  },
+  server: {
+    port: 5050,
+    open: true,
+  },
+});
