f762849ffc2dadff1e6939510aed399d8972ce39
ttjoe <ttjoe0@gmail.com>
2023-04-15 09:41:07 +0000

    [HOTFIX] Home comp. responsive grid cells

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Paper from '@mui/material/Paper';
 import FireFeed from '@/FireFeed';
 import { Box, Container } from '@mui/material';
 import Map from '@/Map';
+import MediaQuery from "react-responsive";
+import { useMediaQuery } from 'react-responsive'
 
 const Item = styled(Paper)`
     && {
@@ -13,20 +15,43 @@ const Item = styled(Paper)`
 `
 
 function Home() {
+    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' })
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
     return (
         <Container maxWidth={'lg'}>
             <h1>Header is coming here</h1>
             <Grid container spacing={1}>
-                <Grid item xs={5}>
-                    <Item>
-                        <FireFeed />
-                    </Item>
-                </Grid>
-                <Grid item xs={7}>
-                    <Item>
-                        <Map />
-                    </Item>
-                </Grid>
+                {
+                    isDesktopOrLaptop && (
+                        <>
+                            <Grid item xs={5} >
+                                <Item>
+                                    <FireFeed />
+                                </Item>
+                            </Grid>
+
+                            <Grid item xs={7}>
+                                <Item>
+                                    <Map />
+                                </Item>
+                            </Grid>
+                        </>
+                    )
+                }
+                {
+                    isTabletOrMobile && (
+                        <>
+                            <Grid item xs={12}>
+                                <Item>
+                                    <FireFeed />
+                                </Item>
+                            </Grid>
+                        </>
+                    )
+                }
             </Grid>
         </Container>
     )
