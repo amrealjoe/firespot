db3dd5f052c2254a034a58a5ea2f4591a54298d9
ttjoe <ttjoe0@gmail.com>
2023-05-25 09:42:53 +0000

    UUPDATE: destructured getAddress response

--- src/helpers/getAddress.js
+++ src/helpers/getAddress.js
@@ -1,19 +1,22 @@
 export const getAddress = async (lat, lng, api_key) => {
-    async function fetchAddress() {
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${api_key}`
-        )
-          .then((response) => response.json())
-          .then((result) => result)
-          .catch((err) => console.log(err));
-        const { results } = response;
-        const { address_components } = results[0];
-        console.log(address_components);
-        let city = address_components[1]["long_name"];
-        const county = address_components[3]["long_name"];
-        const country = address_components[4]["long_name"];
-        return address = { city, county, country };
-    }
-    const r = await fetchAddress()
-    return r
+  async function fetchAddress() {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${api_key}`
+    )
+      .then((response) => response.json())
+      .then((result) => result)
+      .catch((err) => console.log(err));
+    const { results } = response;
+    const { address_components } = results[0];
+    console.log(address_components);
+    let property = "long_name";
+    // const city = address_components[1][property];
+    // const county = address_components[3][property];
+    // const country = address_components[4][property];
+    const { long_name: city } = address_components[1];
+    const { long_name: county } = address_components[3];
+    const { long_name: country } = address_components[4];
+    return { city, county, country };
+  }
+  return (address = await fetchAddress());
 };
